6db649d0deb868ac7ca379d3a98792af73abf721
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-04 19:02:44 +0200

    some more cleanup, could show error in template if "no patents"

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import os
 
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from flask import (Flask, render_template, flash, request,
+                   redirect, url_for, send_from_directory)
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 
-from src.patent_fetcher import (
-    conduct_search, download_patents,
-    get_sample_images, extract_images, display)
+from src.patent_fetcher import (conduct_search, download_patents,
+                                get_sample_images)
 
 load_dotenv()
 
@@ -19,7 +19,6 @@ app = Flask(__name__, template_folder=template_folder)
 app.config['UPLOAD_FOLDER'] = os.getenv("UPLOAD_FOLDER")
 app.config['DOWNLOAD_FOLDER'] = os.getenv("DOWNLOAD_FOLDER")
 app.static_folder = os.getenv("STATIC_FOLDER")
-
 
 
 def allowed_file(filename: str):
@@ -70,7 +69,8 @@ def _create_folder(dir_name):
 def patent_search(patent_title):
     """ Let the user search for a patent by title directly
 
-    Need to figure out how to implement two word searches, e.g. automotive camera
+    Need to figure out how to implement two word searches,
+    e.g. automotive camera
     """
     title = patent_title.replace(" ", "")
     dir_name = str(Path(app.config['DOWNLOAD_FOLDER']) / title)
@@ -80,21 +80,18 @@ def patent_search(patent_title):
     patent_results = conduct_search(patent_title, limit=10)
 
     if not patent_results:
-        # TODO: if this is unexpected use a flask abort(400) or something?
-        print(f'how in the world did we wind up here?!?!? patents={len(patent_results)}')
-        images = [f"{app.static_folder}/patent_image_not_available.png"] * len(patent_results)
-        return render_template('search_results.html', category=patent_title, patents=patent_results,
-                                images=images)
+        return render_template('search_results.html', category=patent_title,
+                               patents=[], error="No patents found")
 
     # Download patent drawings to new folder
     drawing_files = download_patents(patent_results, destination=dir_name)
 
     # Extract images from patents' prior art
-    sample_images = get_sample_images(
-        drawing_files, app.static_folder)
+    sample_images = get_sample_images(drawing_files, app.static_folder)
 
     return render_template('search_results.html', category=patent_title,
-                            patents=patent_results, images=sample_images)
+                           patents=patent_results, images=sample_images,
+                           error=None)
 
 
 if __name__ == '__main__':
